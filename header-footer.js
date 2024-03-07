fbe2e172c9de526a6e4fbb472448993287eb9115
Douimtouch <134249139+Douimtouch@users.noreply.github.com>
2024-03-07 09:41:04 +0100

    Add files via upload

--- header-footer.js
+++ header-footer.js
@@ -1,4 +1,47 @@
+// header-footer.js
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Code spécifique au header et au footer, si nécessaire
+    const menuIcon = document.getElementById('menu-icon');
+    const menuItems = document.getElementById('menu-items');
+    const dropdownItems = document.querySelectorAll('.dropdown');
+    const headerIframe = window.parent.document.querySelector('header iframe');
+
+    function toggleMenu() {
+        menuItems.classList.toggle('active');
+        headerIframe.classList.toggle('expanded');
+        
+        if (headerIframe.classList.contains('expanded')) {
+            headerIframe.style.height = '900px';
+        } else {
+            headerIframe.style.height = '50px';
+        }
+    }
+
+    menuIcon.addEventListener('click', function() {
+        toggleMenu();
+    });
+
+    dropdownItems.forEach(function(item) {
+        item.addEventListener('click', function(event) {
+            event.stopPropagation();
+            this.classList.toggle('active');
+        });
+    });
+
+    document.addEventListener('click', function(event) {
+        const target = event.target;
+        if (!menuIcon.contains(target) && !menuItems.contains(target)) {
+            menuItems.classList.remove('active');
+            headerIframe.classList.remove('expanded');
+            headerIframe.style.height = '50px';
+        }
+    });
+
+    window.addEventListener('resize', function() {
+        if (window.innerWidth > 768) {
+            menuItems.classList.remove('active');
+            headerIframe.classList.remove('expanded');
+            headerIframe.style.height = '50px';
+        }
+    });
 });
